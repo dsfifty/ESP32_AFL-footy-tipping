e033abdac474dea8e852d9bc3ed9113b25e1b7ad
dsfifty <113217855+dsfifty@users.noreply.github.com>
2024-05-31 16:52:12 +0930

    Update Footy_API_v1.02.ino

--- Footy_API_v1.02.ino
+++ Footy_API_v1.02.ino
@@ -103,8 +103,8 @@ int weekDay = 10;         // global variable to hold the day of the week when se
 int hour = 23;
 int tipsLive = 1;  // global variable 1 = tips are retrieved from API, 0= tips are retrieved from LittleFS
 
-char ssid[] = "Jerry";                 // your network SSID (name)
-char password[] = "ABCNEWSBREAKFAST";  // your network key
+char ssid[] = "YOURSSID";                 // your network SSID (name)
+char password[] = "YOURPASSWORD";  // your network key
 
 WiFiClientSecure client;  // starts the client for HTTPS requests
 
@@ -180,7 +180,7 @@ void gameRequest() {  // function to get the games and scores
   client.print(F("Host: "));  //Headers
   client.println(HOST);
   client.println(F("Cache-Control: no-cache"));
-  client.println("User-Agent: Arduino Footy Project - dsfifty.smith@gmail.com");  // Required by API for contact if anything goes wrong
+  client.println("User-Agent: Arduino Footy Project - ds@ds.com");  // Required by API for contact if anything goes wrong
 
   if (client.println() == 0) {
     Serial.println(F("Failed to send request"));
@@ -296,7 +296,7 @@ void tipsRequest() {  // function to get the current tips
     client.print(F("Host: "));  //Headers
     client.println(HOST);
     client.println(F("Cache-Control: no-cache"));
-    client.println("User-Agent: Arduino Footy Project - dsfifty.smith@gmail.com");  // Required by API for contact if anything goes wrong
+    client.println("User-Agent: Arduino Footy Project - ds@ds.com");  // Required by API for contact if anything goes wrong
 
     if (client.println() == 0) {
       Serial.println(F("Failed to send request"));
